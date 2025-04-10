7d0aa4fd82542fdbaa9d8dfbaa76f3b235ec0fc8
Benja0g <benjamj3012@gmail.com>
2025-04-10 19:08:08 -0400

    ACTUALIZACIONES JODER

---
+++ Funciones/Funciones.py
@@ -0,0 +1,6 @@
+#funcion: set de instrucciones que ejecuta una tarea especifica
+#arg: argumentos con los que va a trabajar la función
+def saludar(nombre):
+    print(f"Que pasha tio joder ostias te gusta deftones {nombre}")
+nombre = input ("ingrese su nombre: ")
+saludar(nombre)
--- bucles/bucle_for.py
+++ bucles/bucle_for.py
@@ -2,6 +2,7 @@
 juegos = ["Dota 2", "MK", "street figther", "counter strike", "7DaystoDie"]
 numeros = [10,20,30,40,50]
 
+
 for juego in juegos:
     print(juego)
 
@@ -22,3 +23,31 @@ for elemento in enumerate(numeros):
     indice = elemento[0]
     valor = elemento[1]
     print(f"El indice es: {indice} y el valor es: {valor}")
+    
+diccionario = {
+    "nombre": "Benjamin",
+    "apellido":"Baraona",
+    "edad": 19,
+    "estudiante":True
+}
+
+print()
+for elemento in diccionario:
+    print(f"la clave del dato es: {elemento}")
+   
+print()
+for elemento in diccionario.items():
+    clave = elemento[0]
+    valor = elemento[1]
+    print(f"la clave del dato es: '{clave}' y el valor es: '{valor}'")
+    
+conjunto = {"Aquiles voy",55,True,"comandos",23}
+
+print()
+for elemento in conjunto:
+  if type(elemento) == int:
+      print(elemento)
+      
+#un conjunto no es ordenable por lo que no se puede consultar indice
+
+
