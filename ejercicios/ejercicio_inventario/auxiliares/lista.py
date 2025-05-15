684544a3114746c6153ac1cae04c37b36e8fd56b
CONDREX <benjapaifre@gmail.com>
2025-05-15 19:05:02 -0400

    cambios

---
+++ ejercicios/ejercicio_inventario/auxiliares/lista.py
@@ -0,0 +1,7 @@
+lista_productos =["Mouse Razer Black Widow","Teclado Pizz Pro","Audifonos HyperX Alpha"]
+lista_precios = [60000,55000,134000]
+lista_cantidad = [25,15,3]
+
+print(lista_productos[0],lista_precios[0],lista_cantidad[0])
+print(lista_productos[1],lista_precios[1],lista_cantidad[1])
+print(lista_productos[2],lista_precios[2],lista_cantidad[2])
