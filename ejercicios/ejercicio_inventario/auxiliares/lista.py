1ac31c722f474e9c2ed58fecb59a17b5606a6525
CONDREX <benjapaifre@gmail.com>
2025-05-20 16:20:27 -0400

    hola

--- ejercicios/ejercicio_inventario/auxiliares/lista.py
+++ ejercicios/ejercicio_inventario/auxiliares/lista.py
@@ -5,3 +5,4 @@ lista_cantidad = [25,15,3]
 print(lista_productos[0],lista_precios[0],lista_cantidad[0])
 print(lista_productos[1],lista_precios[1],lista_cantidad[1])
 print(lista_productos[2],lista_precios[2],lista_cantidad[2])
+print("hola")
