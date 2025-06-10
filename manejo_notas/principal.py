166c9ff66eb3e685064bb0de52a4218b017968c3
CONDREX <benjapaifre@gmail.com>
2025-06-10 18:12:40 -0400

    cambios

--- manejo_notas/principal.py
+++ manejo_notas/principal.py
@@ -1,5 +1,6 @@
 from data.asignaturas import asignaturas
 from data.crear_data import crear_data
+import os
 
 # contador = 1
 # for asignarutas in lista_asignaturas:
@@ -10,7 +11,7 @@ def Mostrar_listado_asignaturas():
     print("listado de asignaturas")
     print("=======================")
     contador = 0
-    for asignatura in sorted(lista_asignaturas):
+    for asignatura in sorted(asignaturas):
         contador += 1
         print(f"{contador}.- {asignatura}")
 
@@ -23,16 +24,16 @@ def buscar_asignatura():
 def agregar_asignatura():
     Mostrar_listado_asignaturas()
     nueva_asignatura = input("ingrese nueva asignatura: ")
-    lista_asignaturas.append(nueva_asignatura.title())
+    asignaturas.append(nueva_asignatura.title())
     Mostrar_listado_asignaturas()
 
 def actualizar_asignatura():
     Mostrar_listado_asignaturas()
     busqueda = input("ingrese asignatura a buscar: ")
-    for i in range(len(lista_asignaturas)):
-        if busqueda.lower() in lista_asignaturas[i].lower():
-            nuevo_dato = input(f"Ingrese nuevo nombre para asignatura {lista_asignaturas[i]}: ")
-            lista_asignaturas[i] = nuevo_dato
-    crear_data("asignaturas.py","asignaturas",asignatura)
+    for i in range(len(asignaturas)):
+        if busqueda.lower() in asignaturas[i].lower():
+            nuevo_dato = input(f"Ingrese nuevo nombre para asignatura {asignaturas[i]}: ")
+            asignaturas[i] = nuevo_dato
+    crear_data("asignaturas.py","asignaturas",asignaturas)
     Mostrar_listado_asignaturas()
 actualizar_asignatura()
